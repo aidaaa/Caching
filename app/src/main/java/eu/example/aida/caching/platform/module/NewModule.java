8ea221ecd62456fe1f8033128523f68a3de4bcae
Aida <a.mosayebi478@GMAIL.Com>
2019-01-29 12:49:55 +0330

    Catching ok

--- app/src/main/java/eu/example/aida/caching/platform/module/NewModule.java
+++ app/src/main/java/eu/example/aida/caching/platform/module/NewModule.java
@@ -1,10 +1,21 @@
 package eu.example.aida.caching.platform.module;
 
+import android.content.Context;
+
 import javax.inject.Scope;
 
 import dagger.Module;
 import dagger.Provides;
 import eu.example.aida.caching.app.presenter.Presenter;
+import eu.example.aida.caching.data.RepositoryImpl;
+import eu.example.aida.caching.data.cache.FileManagment;
+import eu.example.aida.caching.data.cache.MoviesCache;
+import eu.example.aida.caching.data.cache.MoviesCacheImpl;
+import eu.example.aida.caching.data.cache.ToJson;
+import eu.example.aida.caching.data.factory.DataStoreFactory;
+import eu.example.aida.caching.data.mapper.MoviesJsonMapper;
+import eu.example.aida.caching.data.net.RestApi;
+import eu.example.aida.caching.data.net.RestApiImpl;
 import eu.example.aida.caching.domain.interactor.base.UseCaseFactory;
 import eu.example.aida.caching.domain.repository.Repository;
 import eu.example.aida.caching.platform.scope.ViewScope;
@@ -31,5 +42,52 @@ public class NewModule
         return new UseCaseFactory(repository);
     }
 
+    @ViewScope
+    @Provides
+    public Repository getRepository(DataStoreFactory dataStoreFactory)
+    {
+        return new RepositoryImpl(dataStoreFactory);
+    }
 
+    @ViewScope
+    @Provides
+    public DataStoreFactory getDataStoreFactory(RestApi restApi, MoviesCache moviesCache)
+    {
+        return new DataStoreFactory(restApi,moviesCache);
+    }
+
+    @ViewScope
+    @Provides
+    public RestApi getRestApi(MoviesJsonMapper mapper)
+    {
+        return new RestApiImpl(mapper);
+    }
+
+    @ViewScope
+    @Provides
+    public MoviesCache getMoviesCache(Context context, FileManagment fileM, ToJson toJson)
+    {
+        return new MoviesCacheImpl(context,fileM,toJson);
+    }
+
+    @ViewScope
+    @Provides
+    public MoviesJsonMapper getMoviesJsonMapper()
+    {
+        return new MoviesJsonMapper();
+    }
+
+    @ViewScope
+    @Provides
+    public FileManagment getFileManagment()
+    {
+        return new FileManagment();
+    }
+
+    @ViewScope
+    @Provides
+    public ToJson getToJson()
+    {
+        return new ToJson();
+    }
 }
